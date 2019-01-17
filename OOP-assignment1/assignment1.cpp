fa8f7e437bfd09dde0fe473c07338431375ddf1b
kingkong2899 <vuhung2881999@gmail.com>
2019-01-17 18:46:20 +0700

    last-commit

--- OOP-assignment1/assignment1.cpp
+++ OOP-assignment1/assignment1.cpp
@@ -3,13 +3,15 @@
 #include "iomanip"
 using namespace std;
 
+class Phieu_VPP;
+
 class DATE{
     int d, m, y;
 public:
     void nhap(){
-        cout << "Ngay: ";        cin >> d;
-        cout << "Thang: ";       cin >> m;
-        cout << "Nam: ";         cin >> y;
+        cout << "Ngay  : ";       cin >> d;
+        cout << "Thang : ";       cin >> m;
+        cout << "Nam   : ";       cin >> y;
     }
     void xuat(){
         cout << "Ngay lap:  " << d << "/ " << m << "/ " << y << endl;
@@ -20,10 +22,10 @@ class NCC{
     string maNCC, tenNCC, diaChi, soDT;
 public:
     void nhap(){
-        cout << "Ma nha cung cap: ";    getline(cin, maNCC);
-        cout << "Ten nha cung cap: ";   getline(cin, tenNCC);
-        cout << "Dia chi: ";            getline(cin, diaChi);
-        cout << "So dien thoai: ";      getline(cin, soDT);
+        cout << "Ma nha cung cap  : ";       getline(cin, maNCC);
+        cout << "Ten nha cung cap : ";       getline(cin, tenNCC);
+        cout << "Dia chi          : ";       getline(cin, diaChi);
+        cout << "So dien thoai    : ";       getline(cin, soDT);
         cin.ignore(1);
     }
     void xuat(){
@@ -38,16 +40,18 @@ class SP{
     float donGia;
 public:
     void nhap(){
-        cout << "Ma san pham: ";    getline(cin, maSP);
-        cout << "Ten san pham: ";   getline(cin, tenSP);
-        cout << "So luong: ";       cin >> soLuong;
-        cout << "Don gia: ";        cin >> donGia;
+        cout << "Ma san pham  : ";       getline(cin, maSP);
+        cout << "Ten san pham : ";       getline(cin, tenSP);
+        cout << "So luong     : ";       cin >> soLuong;
+        cout << "Don gia      : ";       cin >> donGia;
         cin.ignore(1);
     }
     void xuat(){
         cout << setw(15) << maSP << setw(15) << tenSP << setw(15) << soLuong << setw(15) << donGia << setw(15) << (float)soLuong*donGia << endl;
     }
     friend class Phieu_VPP;
+    friend int dem_SoLuong(Phieu_VPP check);
+    friend void sapXep_DonGia(Phieu_VPP sort);
 };
 
 class Phieu_VPP{
@@ -57,9 +61,9 @@ class Phieu_VPP{
     SP *sanPham;    int n;
 public:
     void nhap(){
-        cout << endl << "Ten truong dai hoc: ";     getline(cin, tenDH);
+        cout << endl << "Ten truong dai hoc : ";     getline(cin, tenDH);
         cout << endl << "NHAP THONG TIN PHIEU VAN PHONG PHAM" << endl;
-        cout << endl << "Ma phieu: ";   getline(cin, maPhieu);
+        cout << endl << "Ma phieu           : ";     getline(cin, maPhieu);
         cout << endl << "NGAY THANH LAP" << endl;   ngayLap.nhap();     cin.ignore(1);
         cout << endl << "NHA CUNG CAP" << endl;     nhaCC.nhap();
         cout << endl << "THONG TIN SAN PHAM" << endl;
@@ -71,6 +75,7 @@ public:
         }
     }
     void xuat(){
+        cout << endl << "-----------------------------------------------------------------------------" << endl;
         cout << endl << endl << "Dai hoc " << tenDH << endl;
         cout << setw(45) << "PHIEU NHAP VAN PHONG PHAM" << endl << endl;
         cout << "Ma phieu:        " << maPhieu << setw(25);    ngayLap.xuat();
@@ -81,14 +86,44 @@ public:
             sanPham[i].xuat();
             tong += sanPham[i].soLuong*sanPham[i].donGia;
         }
-        cout << setw(37) << "TONG" << setw(38) << tong << endl << endl;
-        cout << setw(15) << "Hieu truong" << setw(25) << "Phong tai chinh" << setw(25) << "Nguoi lap" << endl << endl;
+        cout << endl << setw(37) << "TONG" << setw(38) << tong << endl << endl;
+        cout << setw(20) << "Hieu truong" << setw(25) << "Phong tai chinh" << setw(25) << "Nguoi lap" << endl << endl;
+        cout << "-----------------------------------------------------------------------------" << endl;
     }
+    friend int dem_SoLuong(Phieu_VPP check);
+    friend void sapXep_DonGia(Phieu_VPP sort);
 };
+
+int dem_SoLuong(Phieu_VPP check){
+    int dem = 0;
+    for(int i=0; i<check.n; i++){
+        if(check.sanPham[i].soLuong<80)    dem++;
+    }
+    return dem;
+}
+
+void doiCho(SP &a, SP &b){
+    SP tg = a;  a = b;  b = tg;
+}
+
+void sapXep_DonGia(Phieu_VPP sort){
+    for(int i=1; i<sort.n; i++){
+        for(int j=0; j<sort.n-i; j++){
+            if(sort.sanPham[j].donGia > sort.sanPham[j+1].donGia){
+                doiCho(sort.sanPham[j], sort.sanPham[j+1]);
+            }
+        }
+    }
+}
 
 int main(){
     Phieu_VPP a;
     a.nhap();
+    a.xuat();
+    cout << endl << "- So san pham co so luong nhap nho hon 80 trong danh sach la: " << dem_SoLuong(a) << endl;
+    cout << "-----------------------------------------------------------------------------" << endl;
+    cout << endl << "- Phieu van phong pham sau khi duoc sap xep theo don gia :" << endl;
+    sapXep_DonGia(a);
     a.xuat();
     return 0;
 }
