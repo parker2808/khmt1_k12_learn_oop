3020279201444a9a8474f91e1fb825fb7ee74ef9
kingkong2899 <vuhung2881999@gmail.com>
2019-04-28 17:08:44 +0700

    update constructor

--- OOP-OnTap/All_Units/bth05/bai2.cpp
+++ OOP-OnTap/All_Units/bth05/bai2.cpp
@@ -17,8 +17,11 @@ public:
         year = c;
     }
     void xuat(){
-        cout << setw(20) << day << "/ " << month << "/ " << year;
+        cout << setw(11) << day << "/ " << month << "/ " << year;
     }
+    friend class list;
+    friend class hangHoa;
+    friend long tongThanhTienThang8(list a);
 };
 
 class hangHoa{
@@ -43,8 +46,12 @@ public:
     void xuat(){
         cout << setw(20) << maHang << setw(20) << tenHang;
         ngayNhap.xuat();
-        cout << setw(20) << soLg << setw(20) << donGia << endl;
+        cout << setw(20) << soLg << setw(20) << donGia;
     }
+    friend class list;
+    friend long tongThanhTienThang8(list a);
+    friend long findMax(list x);
+    friend void hienThiDonGiaMax(list x);
 };
 
 class list{
@@ -55,16 +62,81 @@ public:
         cout << "n = "; cin >> n;   cin.ignore(1);
         a = new hangHoa[n];
         for(int i=0; i<n; i++){
-            
+            cout << "- Mat hang thu " << i+1 << endl;
+            cout << "Ma hang: ";    cin >> a[i].maHang; cin.ignore(1);
+            cout << "Ten hang: ";   cin.getline(a[i].tenHang, 65);
+            cout << "- Date" << endl;
+            cout << "Ngay: ";   cin >> a[i].ngayNhap.day;
+            cout << "Thang: ";  cin >> a[i].ngayNhap.month;
+            cout << "Nam: ";    cin >> a[i].ngayNhap.year;
+            cout << "So luong: ";   cin >> a[i].soLg;
+            cout << "Don gia: ";    cin >> a[i].donGia;
+            cin.ignore(1);
         }
     }
     ~list(){
         n = 0;
         delete a;
     }
+    void xuat(){
+        cout << setw(20) << "MaHang" << setw(20) << "TenHang" << setw(20)
+             << "NgayNhap" << setw(20) << "SoLuong" << setw(20) << "DonGia" << setw(20)
+             << "ThanhTien" << endl;
+        for(int i=0; i<n; i++){
+            a[i].xuat();
+            cout << setw(20) << a[i].soLg * a[i].donGia << endl;
+        }
+    }
+    friend long tongThanhTienThang8(list a);
+    friend long findMax(list x);
+    friend void hienThiDonGiaMax(list x);
 };
 
-int main(){
+long tongThanhTienThang8(list x){
+    long thanhTien = 0;
+    for(int i=0; i<x.n; i++){
+        if(x.a[i].ngayNhap.month == 8 && x.a[i].ngayNhap.year == 2017)
+            thanhTien += (x.a[i].soLg * x.a[i].donGia);
+    }
+    return thanhTien;
+}
+
+long findMax(list x){
+    long aMax = x.a[0].donGia;
+    for(int i=0; i<x.n; i++){
+        if(x.a[i].donGia>aMax)   aMax = x.a[i].donGia;
+    }
+    return aMax;
+}
+
+void hienThiDonGiaMax(list x){
+    cout << setw(20) << "MaHang" << setw(20) << "TenHang" << setw(20)
+         << "NgayNhap" << setw(20) << "SoLuong" << setw(20) << "DonGia" << setw(20)
+         << "ThanhTien" << endl;
+    for(int i=0; i<x.n; i++){
+        if(x.a[i].donGia == findMax(x)){
+            x.a[i].xuat();
+            cout << setw(20) << x.a[i].soLg * x.a[i].donGia << endl;
+        }
+    }
+}
+
+void swap(hangHoa &a, hangHoa &b){
+    hangHoa tg = a;
+    a = b;
+    b = tg;
+}
+
+void sapXepGiamThanhTien(list x){
     
+}
+
+int main(){
+    list a;
+    cout << endl << setw(50) << "---- Danh sach mat hang vua nhap ----" << endl;    a.xuat();
+    cout << "- Tong thanh tien cac mat hang duoc nhap vao thang 8 nam 2017 la: " << tongThanhTienThang8(a) << endl;
+    cout << "- Danh sach cac mat hang co don gia cao nhat" << endl; hienThiDonGiaMax(a);
+
+    a.~list();
     return 0;
 }
